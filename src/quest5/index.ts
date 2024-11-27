f57b358d161d52e9faf8a0464dbba1a7301e28ed
Dakota Mumford <dakotamum@gmail.com>
2024-11-27 00:21:56 -0700

    quest 5 part 2 working with example, not with input

--- src/quest5/index.ts
+++ src/quest5/index.ts
@@ -17,26 +17,24 @@ function part1() {
   let totalRounds = 10;
 
   while (roundNum < totalRounds) {
-    console.log("******************** Round: ", roundNum + 1);
     let clapper = columns[roundNum % columns.length].shift();
     if (clapper === undefined) {
       return;
     }
 
     let nextColumn = columns[(roundNum + 1) % columns.length];
-    let isLeftSide: boolean = Math.floor(clapper / nextColumn.length) % 2 === 0;
+    let divisor = Math.floor(clapper / nextColumn.length);
+    let modulo = clapper % nextColumn.length;
+    let isLeftSide = (((divisor % 2 === 0 && modulo != 0) || ( divisor % 2 != 0 && modulo === 0)) ? true : false);
+    
     let offSet = clapper % nextColumn.length;
     if (isLeftSide) {
       nextColumn.splice(offSet - 1, 0, clapper);
-      console.log("is left side!");
     } else {
-      nextColumn.splice(nextColumn.length - offSet + 1, 0, clapper);
-      console.log("is right side!");
+      nextColumn.splice(offSet + 1, 0, clapper);
     }
-    console.log(nextColumn);
 
-    // get first number of each column as a string with no spaces
-    let result = columns.map((column) => column[0]).join("");
+    let result = parseInt(columns.map((column) => column[0]).join(""));
     console.log(result);
 
     ++roundNum;
@@ -45,6 +43,55 @@ function part1() {
 
 function part2() {
   let input = input2;
+
+  const rows = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split(" ").map(Number));
+  const columns: number[][] = [];
+  for (let i = 0; i < rows[0].length; i++) {
+    const column: number[] = rows.map((row) => row[i]);
+    columns.push(column);
+  }
+
+  let roundNum = 0;
+  let frequencyMap = new Map<number, number>();
+
+  while (true) {
+    let clapper = columns[roundNum % columns.length].shift();
+    if (clapper === undefined) {
+      return;
+    }
+
+    let nextColumn = columns[(roundNum + 1) % columns.length];
+    let divisor = Math.floor(clapper / nextColumn.length);
+    let modulo = clapper % nextColumn.length;
+    let isLeftSide = (((divisor % 2 === 0 && modulo != 0) || ( divisor % 2 != 0 && modulo === 0)) ? true : false);
+    
+    let offSet = clapper % nextColumn.length;
+    if (isLeftSide) {
+      nextColumn.splice(offSet - 1, 0, clapper);
+    } else {
+      nextColumn.splice(offSet + 1, 0, clapper);
+    }
+
+    // get first number of each column as a string with no spaces
+    let result = parseInt(columns.map((column) => column[0]).join(""));
+
+    if (frequencyMap.has(result) && frequencyMap.get(result) !== undefined)
+    {
+      frequencyMap.set(result, frequencyMap.get(result)! + 1);
+      if (frequencyMap.get(result) == 2024)
+      {
+        console.log(result * (roundNum + 1));
+        break;
+      }
+    }
+    else
+      frequencyMap.set(result, 1);
+
+    ++roundNum;
+  }
 }
 
 function part3() {
